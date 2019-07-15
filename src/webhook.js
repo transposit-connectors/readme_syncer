7e389a43bd6ebcf4822166db1319aca960a22587
tina <tina@transposit.com>
2019-07-15 03:45:58 +0000

    Commit from Transposit IDE

--- src/webhook.js
+++ src/webhook.js
@@ -1,18 +1,30 @@
-({ http_event }) => {
-  let {user_id, team_id, response_url} = http_event.parsed_body;
-  setImmediate(() => {
-    let user = `${team_id}:${user_id}`;
-    let message = api.run('this.get_slack_message', {user})[0];
-    console.log(message);
-    api.run("slack_webhook.post_to_response_url", {
-      response_url: response_url,
-      post_body: message
-    });
-  });
-  return { status_code: 200 };
-}
-
-/*
+/**
+ * This operation is an example of a JavaScript operation deployed as a Webhook
+ * and configured to work with Slack.
+ *
  * For sample code and reference material, visit
  * https://www.transposit.com/docs/building/webhooks
  */
+({ http_event }) => {
+  const parsed_body = http_event.parsed_body;
+  const workspaceId = parsed_body.team_id;
+  const userId = parsed_body.user_id;
+  const response_url = parsed_body.response_url;
+
+  setImmediate(() => {
+    let user = api.user({type: "slack", workspaceId, userId});
+    if (user) {
+      let message = api.run('this.get_slack_message', {}, {asUser: user.id})[0];
+      api.run("slack_webhook.post_to_response_url", {
+        response_url: response_url,
+        post_body: message
+      });      
+    } else {
+      api.run("slack_webhook.post_to_response_url", {
+        response_url: response_url,
+        post_body: {text: 'Please configure your user'}
+      });      
+    }
+  });
+  return { status_code: 200 };
+}
