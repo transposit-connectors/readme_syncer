ed4654932c222ec37ecbda81463f7847e20c3468
Daisie Huang <daisie@transposit.com>
2019-08-07 06:01:21 +0000

    Commit from Transposit IDE

--- src/webhook.js
+++ src/webhook.js
@@ -30,7 +30,7 @@
     } else {
       api.run("slack_webhook.post_to_response_url", {
         response_url: response_url,
-        post_body: {text: 'Please configure your user at https://github-transfer-pkk36.transposit.io'}
+        post_body: {text: 'Please configure your user at ' + ${env.getBuiltin().appUrl}}
       });      
     }
   });
