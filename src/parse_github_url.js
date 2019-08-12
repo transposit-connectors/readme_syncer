2f3d45a7c9a58f815c6c134320620a00f4414d45
tina <tina@transposit.com>
2019-08-12 22:40:30 +0000

    make branch a param

--- src/parse_github_url.js
+++ src/parse_github_url.js
@@ -1,11 +1,14 @@
-({url}) => {
+({url, branch}) => {
     // e.g. https://github.com/transposit-connectors/applicant_tracker/blob/master/README.md
-    var url_match = /(https:\/\/)*(github.com\/)*(.+?)\/(.+?)\/blob\/(.+?)\/(.+)/.exec(url);
+    var regex = new RegExp("(https:\/\/)*(github.com\/)*(.+?)\/(.+?)\/blob\/" + branch + "\/(.+)");
+    // e.g. https://github.com/transposit-connectors/applicant_tracker/blob/master/README.md
+    // var url_match = regex.exec(url);
+    var url_match = regex.exec(url);
     if (url_match) {
       return { owner: url_match[3], 
                repo: url_match[4],
-               branch: url_match[5],
-               path: url_match[6]
+               branch: branch,
+               path: url_match[5]
              };
     }
     return null;
