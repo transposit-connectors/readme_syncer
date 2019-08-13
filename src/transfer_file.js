438689274662abdfdec14623601436d7772b293b
tina <tina@transposit.com>
2019-08-13 18:03:07 +0000

    Commit from Transposit IDE

--- src/transfer_file.js
+++ src/transfer_file.js
@@ -1,41 +1,52 @@
-(params) => {
+({app_name, from_app}) => {
   let docs_branch = env.get('docs_branch');
-  var app = api.run('this.parse_github_url', {url: params.app_url, branch: 'master'})[0];
-  var docs = api.run('this.parse_github_url', {url: params.docs_url, branch: docs_branch})[0];
+  var app = {
+    "owner": "transposit-connectors",
+    "repo": app_name,
+    "branch": "master",
+    "path": "README.md"
+  };
+  var docs = {
+    "owner": "transposit",
+    "repo": "www",
+    "branch": docs_branch,
+    "path": `${app_name}.md`
+  };
+
+  let source = from_app ? app : docs;
+  let target = from_app ? docs : app;
+
   var github_user = api.run('github.get_user_authenticated', {})[0];
 
-  console.log(github_user)
-  if (app && docs) {
-    console.log(app)
-    var source_blob = api.run("this.find_blob_object", { owner: app.owner, path: app.path, repo: app.repo, branch: app.branch, user: params.user })[0];
-    var target_blob = api.run("this.find_blob_object", { owner: docs.owner, path: docs.path, repo: docs.repo, branch: docs.branch, user: params.user })[0];
+  var source_blob = api.run("this.find_blob_object", { owner: source.owner, path: source.path, repo: source.repo, branch: source.branch })[0];
+  var target_blob = api.run("this.find_blob_object", { owner: target.owner, path: target.path, repo: target.repo, branch: target.branch })[0];
 
-    // check to see that source file exists:
-    if (source_blob == null) {
-      return "Couldn't copy github url " + params.app_url + " because the url is invalid.";
-    }
-    var body = { committer: { name: github_user.login, email: github_user.email },
-      message: "copied from " + params.app_url,
-      branch: docs.branch,
-      content: source_blob.content
-    };
-    if (target_blob) {
-      body["sha"] = target_blob.sha;
-    }
-    try {
-      api.run("github.add_file_to_repo", { owner: docs.owner, path: docs.path, repo: docs.repo, $body: body });
-    } catch(e) {
-      return "Couldn't copy github url " + params.app_url + " to " + params.docs_url + ": " + e;
-    }
-  } else {
-    if (app == null) {
-      return params.app_url + " isn't a valid Github url.";
-    }
-    if (docs == null) {
-      return params.docs_url + " isn't a valid Github url.";
-    }
+  // check to see that source file exists:
+  if (source_blob == null) {
+    return "Invalid source";
   }
-  return "Copied " + params.app_url + " to " + params.docs_url + "!";
+  var userEmail = user_setting.get('committer_email');
+  var commitEmail = userEmail ? userEmail : github_user.email;
+  if (commitEmail == null) {
+    return "Please set an email address: " + env.getBuiltin().appUrl;
+  }
+  var body = { committer: { name: github_user.login, email: commitEmail},
+    message: `Copied from ${source.owner}/${source.repo} on branch ${source.branch}`,
+    branch: target.branch,
+    content: source_blob.content
+  };
+  if (source_blob.sha === target_blob.sha) {
+    return `Skipped commit since files are the same`;
+  }
+  if (target_blob) {
+    body["sha"] = target_blob.sha;
+  }
+  try {
+    api.run("github.add_file_to_repo", { owner: target.owner, path: target.path, repo: target.repo, $body: body });
+  } catch(e) {
+    return e;
+  }
+  return `Copied from https://github.com/${source.owner}/${source.repo}/blob/${source.branch}/${source.path} to https://github.com/${target.owner}/${target.repo}/blob/${target.branch}/${target.path}`;
 }
 
 /*
