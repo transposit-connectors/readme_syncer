2d0d45053252adddef489abd9a835ba69dab1c07
tina <tina@transposit.com>
2019-08-14 22:36:59 +0000

    Commit from Transposit IDE

--- src/test_parser.js
+++ src/test_parser.js
@@ -1,9 +1,9 @@
 (params) => {
   let blob = api.run('this.find_blob_object', {
-    owner: 'transposit-connectors',
-    repo: 'applicant_tracker',
-    branch: 'master',
-    path: 'README.md'
+    owner: 'transposit',
+    repo: 'www',
+    branch: 'jpwain/browse-apps',
+    path: '/src/apps/a/applicant_tracker.md'
   })[0];
   let content = blob.content;
   return api.run('front_matter_parser.parse', {"$body.content": content});
